f41300cb6053cda7d7cfa3e9cfbdf2d9c80744ce
Lina-Alrashid <AlrashidLina77@gmail.com>
2024-05-18 04:32:27 +0300

    editing seller account and profile

--- Freelancer/main_app/models.py
+++ Freelancer/main_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Seller_Account(models.Model):
+    syriatel_cash = models.BooleanField(default=False)
+    usdt = models.BooleanField(default=False)
+    al_haram = models.BooleanField(default=False)
+    id_pecture = models.ImageField()
+    def serialize(self): 
+        return {
+            "payment_method": self.payment_method,
+            "id_picture": self.id_picture,
+            "work_group" : self.work_group,
+        }
+    
 class Profile(models.Model):
     language = models.CharField(max_length=50)
     work_group = models.CharField(max_length=50)
@@ -15,5 +26,6 @@ class Profile(models.Model):
             "provided_services": self.provided_services,
             "member_since": self.member_since
         }
+    seller_account = models.ForeignKey(Seller_Account, on_delete=models.CASCADE,related_name='profile')
     #collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     #promotions = models.ManyToManyField(Promotion)
