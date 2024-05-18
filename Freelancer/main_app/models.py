36665692aa91354e15a9cc9bc28c32fded2136a3
Lina-Alrashid <AlrashidLina77@gmail.com>
2024-05-19 01:17:10 +0300

    finishing seller and profile models and urls

--- Freelancer/main_app/models.py
+++ Freelancer/main_app/models.py
@@ -1,21 +1,34 @@
 from django.db import models
 
 class Seller_Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50,null=True)
+    country = models.CharField(max_length=50)
+    bdate = models.DateField()
+    password = models.CharField(max_length=8)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10)
     syriatel_cash = models.BooleanField(default=False)
     usdt = models.BooleanField(default=False)
     al_haram = models.BooleanField(default=False)
     id_picture = models.CharField(max_length=255,null=True)
     def serialize(self): 
         return {
+            "first_name": self.first_name,
+            "second_name": self.second_name,
+            "country": self.country,
+            "bdate": self.bdate,
+            "password": self.password,
+            "email": self.email,
+            "phone_number": self.phone_number,
             "syriatel_cash": self.syriatel_cash,
             "usdt": self.usdt,
             "al_haram": self.al_haram,
             "id_picture": self.id_picture,
-            "work_group" : self.profile.first().work_group,
-            "rate" : self.profile.first().rate
         }
     
 class Profile(models.Model):
+    profile_seller_id = models.IntegerField(default=1)
     language = models.CharField(max_length=50)
     work_group = models.CharField(max_length=50)
     bio = models.TextField(null=True)
--- Freelancer/main_app/urls.py
+++ Freelancer/main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('profile/<int:id>',views.profile, name='profile'),
-    path('account/seller/<int:id>' , views.seller_account , name='seller_account')
+    path('account/seller/<int:id>' , views.seller_account , name='seller_account'),
+    path('account/seller/<int:id>/profile/<int:id1>' , views.seller_profile , name='seller_profile')
 
 ]
