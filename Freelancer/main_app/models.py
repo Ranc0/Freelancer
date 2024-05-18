6265a8ef6e12b35e69c1d839dd2e8b55573a62b5
Lina-Alrashid <AlrashidLina77@gmail.com>
2024-05-18 21:19:04 +0300

    first commit

--- Freelancer/main_app/models.py
+++ Freelancer/main_app/models.py
@@ -4,20 +4,23 @@ class Seller_Account(models.Model):
     syriatel_cash = models.BooleanField(default=False)
     usdt = models.BooleanField(default=False)
     al_haram = models.BooleanField(default=False)
-    id_picture = models.ImageField()
+    id_picture = models.ImageField(null=True)
     def serialize(self): 
         return {
             "payment_method": self.payment_method,
             "id_picture": self.id_picture,
             "work_group" : self.profile.work_group,
+            "rate" : self.profile.rate
         }
     
 class Profile(models.Model):
     language = models.CharField(max_length=50)
     work_group = models.CharField(max_length=50)
-    bio = models.TextField()
-    provided_services = models.IntegerField()
+    bio = models.TextField(null=True)
+    provided_services = models.IntegerField(default=0)
     member_since = models.DateTimeField(auto_now=True)
+    rate = models.FloatField(default=0.0)
+    seller_account = models.ForeignKey(Seller_Account, on_delete=models.CASCADE, default = 0,related_name='profile' )
     def serialize(self):
         return {
             "language": self.language,
@@ -26,6 +29,6 @@ class Profile(models.Model):
             "provided_services": self.provided_services,
             "member_since": self.member_since
         }
-    seller_account = models.ForeignKey(Seller_Account, on_delete=models.CASCADE,related_name='profile' , default = 1)
+    
     #collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     #promotions = models.ManyToManyField(Promotion)
