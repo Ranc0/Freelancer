7031a7914e17662b94f8cbf7e88edb7070e03b63
Ranc0 <raniali12813@gmail.com>
2024-05-18 03:02:24 +0300

    profile view added

--- Freelancer/main_app/views.py
+++ Freelancer/main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index (request) :
-    return render (request, 'index.html')
+    return render (request , 'index.html')
+
+def profile (request , id) :
+     if request.method == "GET": 
+        info = Profile.objects.get(id = id)
+        return JsonResponse(info.serialize())
+   
